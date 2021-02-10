f3177adee9bc62063e4ca0549fc03b401a651147
Javier Bisbal <javierbisbal@Javiers-MacBook-Pro.local>
2021-02-10 12:12:49 -0300

    cambio init

--- pybalu/img_processing/__init__.py
+++ pybalu/img_processing/__init__.py
@@ -1,5 +1,5 @@
+from .snd_deriv import snd_deriv
 from .fst_deriv import fst_deriv
 from .morphoreg import morphoreg
-from .snd_deriv import snd_deriv
 from .rgb2hcm import rgb2hcm
 from .segbalu import segbalu
